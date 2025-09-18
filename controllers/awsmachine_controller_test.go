1c9d756712b18e7e2bb6a6dabd1ec3fa60ca70ab
Josh Branham <jbranham@redhat.com>
2025-09-18 10:13:18 -0600

    UPSTREAM: <carry>: revert Only tag NetworkInterfaces in RunInstances if IAM Allows It
    This reverts commit 7214e8279716b0222e9a59b3dc992afde1716f1f.

--- controllers/awsmachine_controller_test.go
+++ controllers/awsmachine_controller_test.go
@@ -22,7 +22,6 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/elb"
 	"github.com/golang/mock/gomock"
@@ -574,7 +573,6 @@ func mockedCreateInstanceCalls(m *mocks.MockEC2APIMockRecorder) {
 			CreationDate: aws.String("2019-02-08T17:02:31.000Z"),
 		},
 	}}, nil)
-	m.RunInstances(gomock.Any()).Return(nil, awserr.New("DryRunOperation", "", nil))
 	m.RunInstances(gomock.Any()).Return(&ec2.Reservation{
 		Instances: []*ec2.Instance{
 			{
